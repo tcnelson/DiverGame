50f29b4b4660723388eae64287480434eccccee1
tcnelson <Trax1315@gmail.com>
2015-05-16 18:32:12 -0700

    shot destroyed on collision

---
+++ Assets/Scripts/DestroyByCollision.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+function OnCollisionEnter2D(hit : Collision2D){
+	//Destroy(gameObject);
+}
+
+function OnTriggerEnter2D(hit : Collider2D){
+	if (hit.transform.tag == "Player") {
+		return; 
+	}
+	
+	Destroy(gameObject);
+}
