2aa90d122949558aca4bb57374d13c2edea988fb
tcnelson <Trax1315@gmail.com>
2015-05-18 10:51:09 -0700

    created enemy tag; objects with enemy tag are destroyed on contact with shot

--- Assets/Scripts/DestroyByCollision.js
+++ Assets/Scripts/DestroyByCollision.js
@@ -5,12 +5,16 @@ function OnCollisionEnter2D(hit : Collision2D){
 }
 
 function OnTriggerEnter2D(hit : Collider2D){
-	if (hit.transform.tag == "Player" ) {
+	if (hit.transform.tag == "Player" ) {				// Early return if shot hits player
 		return; 
 	}
-	else if (hit.transform.tag == "Boundary") {
+	else if (hit.transform.tag == "Boundary") {			// Early return to ignore collisions w/ Boundary
 		return;
 	}
+	else if (hit.transform.tag == "Enemy") {
+		Destroy(gameObject);							// Destroy shot on collision with enemy
+		Destroy(hit.gameObject);						// Destroy enemy on collision with shot
+	}
 	
-	Destroy(gameObject);
+	Destroy(gameObject);								// Destroy shot on collision with other game object
 }
