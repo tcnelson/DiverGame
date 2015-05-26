d01afa7a1629a60300c182ea0f0b6f78f6b2349f
John Knoll <jpknoll@ucdavis.edu>
2015-05-25 18:27:13 -0700

    chase the player

--- Assets/Scripts/EnemyController.js
+++ Assets/Scripts/EnemyController.js
@@ -1,9 +1,29 @@
 ﻿#pragma strict
 
+var speed : int;
+
+private var player : GameObject;
+private var enemySight : EnemySight;
+private var myRigidBody : Rigidbody2D;
+
+function Awake () {
+	enemySight = GetComponent(EnemySight);
+	player = GameObject.FindGameObjectWithTag("Player");
+	myRigidBody = GetComponent (Rigidbody2D);
+}
+
 function Start () {
 
 }
 
 function Update () {
 
+	// If the player is in sight and is alive...
+    if(enemySight.playerInSight)
+        Chase();
+}
+
+function Chase () {
+	var distance : Vector2 = (player.transform.position - transform.position);
+    myRigidBody.AddForce(distance * speed);
 }
