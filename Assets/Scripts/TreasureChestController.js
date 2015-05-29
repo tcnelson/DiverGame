3506747987735ac2447cf7730264de93d5c93cb0
tcnelson <Trax1315@gmail.com>
2015-05-29 10:49:43 -0700

    treasure chest has a collider, will delete after a delay after being hit by player; open animation still not triggering though

---
+++ Assets/Scripts/TreasureChestController.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+var deleteDelay: float;							// Pause before deleting the treasure chest game object (after it's been opened)
+
+private var animator: Animator; 				// The animator attached to the treasure chest
+
+function Awake () {
+	animator = GetComponent(Animator);
+}
+
+function Start () {
+	animator.SetBool("IsPickedUp", false);		// With the "IsPickedUp" parameter false, the treasure chest will be idle
+}
+
+function OnTriggerEnter2D(hit : Collider2D){
+	if (hit.transform.tag == "Player" ) {	
+		animator.SetBool("IsPickedUp", true);	// Sets "IsPickedUp" to true on contact with player to trigger opening animation
+		Destroy(gameObject, deleteDelay);		// Destroy treasure chest game object after a delay
+	}
+}
