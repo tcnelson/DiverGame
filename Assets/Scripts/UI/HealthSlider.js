ac57ef5d8a79e546da309280c8003c1eaae9d992
John Knoll <jpknoll@ucdavis.edu>
2015-05-29 16:19:08 -0700

    put slider in control of itself

---
+++ Assets/Scripts/UI/HealthSlider.js
@@ -0,0 +1,13 @@
+﻿#pragma strict
+
+private var healthSlider : UnityEngine.UI.Slider;       // Reference to the UI's health bar.
+private var playerHealth : PlayerHealth;
+
+function Awake () {
+	healthSlider = GetComponent(UnityEngine.UI.Slider);
+	playerHealth = GameObject.FindGameObjectWithTag("GameController").GetComponent(PlayerHealth);
+}
+
+function Update () {
+    healthSlider.value = playerHealth.currentHealth;
+}
