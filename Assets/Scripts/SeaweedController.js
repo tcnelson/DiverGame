dc3688c668498300e7f46ae1c84801dc1ea37364
John Knoll <jpknoll@ucdavis.edu>
2015-05-31 15:02:35 -0700

    new seaweed controller

---
+++ Assets/Scripts/SeaweedController.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+private var collider : Collider2D;
+
+function Awake() {
+}
+
+function Hit() {
+	Shrink();
+}
+
+function Shrink() {
+	collider.isTrigger = false;
+}
+
+function Grow() {
+	collider.isTrigger = true;
+}
