718a6df9d71a1b5297c0535cc2d1812a99093622
John Knoll <jpknoll@ucdavis.edu>
2015-05-31 14:16:57 -0700

    silly whale, why you so big

--- Assets/Scripts/FriendlyController.js
+++ Assets/Scripts/FriendlyController.js
@@ -1,11 +1,11 @@
 ﻿#pragma strict
 
 private var animator: Animator;
-private var myRigidbody: Rigidbody2D;
+private var spritePather : SpritePather;
 
 function Awake () {
 	animator = GetComponent (Animator);
-	myRigidbody = GetComponent (Rigidbody2D);
+	spritePather = GetComponent(SpritePather);
 }
 function Start () {
 	animator.SetInteger("Direction", 0);
@@ -16,7 +16,7 @@ function Update () {
 }
 
 function SetAnimationState () {
-	var horizontal = myRigidbody.velocity.x;
+	var horizontal = spritePather.velocity.x;
 	
 	if (horizontal < 0){
 		animator.SetInteger("Direction", 1);
---
+++ Assets/Scripts/Helpers/SpritePather.js
@@ -0,0 +1,23 @@
+﻿#pragma strict
+
+var pathName : String;
+var pathTime : float;
+
+var previousLoc : Vector2;
+var velocity : Vector2;
+
+function Start() {
+	previousLoc = transform.position;
+	Patrol();
+}
+
+function Update(){
+	var currentLoc = transform.position;
+	velocity = (currentLoc - previousLoc) / Time.deltaTime;
+	
+	previousLoc = currentLoc;
+}
+
+function Patrol(){
+	iTween.MoveTo(gameObject, iTween.Hash("path", iTweenPath.GetPath(pathName), "time", pathTime, "easeType", "easeInOutSine", "loopType", "pingPong", "delay", .1));
+}
