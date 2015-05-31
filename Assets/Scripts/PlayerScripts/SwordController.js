d4b9369db7aa63e455e21dfc9caec6acef3b6c55
John Knoll <jpknoll@ucdavis.edu>
2015-05-31 15:02:24 -0700

    damage enemies and seaweed

--- Assets/Scripts/PlayerScripts/SwordController.js
+++ Assets/Scripts/PlayerScripts/SwordController.js
@@ -11,3 +11,20 @@ function Start () {
 function Update () {
 
 }
+
+function Swing() {
+}
+
+function OnCollisionEnter2D(other : Collider2D) {
+	if (other.transform.tag == "Seaweed") {
+		var controller = other.GetComponent(SeaweedController);
+		controller.Hit();
+		return;
+	}
+	
+	if (other.transform.tag == "Enemy") {
+		var controller = other.GetComponent(EnemyController);
+		controller.Damage(20);
+		return;
+	}
+}
