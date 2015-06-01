a192a9268adcb5f0883d44663a623f5654b1068b
John Knoll <jpknoll@ucdavis.edu>
2015-05-31 19:51:46 -0700

    working score

--- Assets/Scripts/TreasureChestController.js
+++ Assets/Scripts/TreasureChestController.js
@@ -1,18 +1,21 @@
-﻿#pragma strict
+#pragma strict
 
 var deleteDelay: float;								// Pause before deleting the treasure chest game object (after it's been opened)
 var idleSprite : Sprite;
 var openSprite : Sprite;
 
 private var spriteRenderer : SpriteRenderer;
+private var score: Score;
 
 function Awake () {
 	spriteRenderer = GetComponent(SpriteRenderer);	// The sprite renderer for the treasure chest
+	score = GameObject.FindGameObjectWithTag("GameController").GetComponent(Score);
 }
 
 function OnTriggerEnter2D(hit : Collider2D){
 	if (hit.transform.tag == "Player" ) {			// Change sprite to open version on contact with player's collider
 		spriteRenderer.sprite = openSprite;
+		score.Add(100);
 		Destroy(gameObject, deleteDelay);			// Destroy treasure chest game object after a delay
 	}
 }
