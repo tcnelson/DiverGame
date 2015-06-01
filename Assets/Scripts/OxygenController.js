029bb92a84dd22d1f0f6f2816d67628d5fe15510
tcnelson <Trax1315@gmail.com>
2015-05-31 22:17:28 -0700

    Small fixes

--- Assets/Scripts/OxygenController.js
+++ Assets/Scripts/OxygenController.js
@@ -4,17 +4,21 @@ var destroyDelay : float;
 var oxygenHealth : float;
 
 private var playerHealth : PlayerHealth;
+private var oxygenSound : AudioSource;
 
 function Awake() {
 	playerHealth = GameObject.FindGameObjectWithTag("GameController").GetComponent(PlayerHealth);
 
 	oxygenHealth = Random.Range(10, 20);
+	
+	oxygenSound = GetComponent(AudioSource);
 
 }
 
 function OnTriggerEnter2D (hit : Collider2D) {
 	if (hit.transform.tag == "Player") {
 		playerHealth.Heal(oxygenHealth);
+		oxygenSound.Play();
 		Destroy(gameObject, destroyDelay);
 	}	
 }
